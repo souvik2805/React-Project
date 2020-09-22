4efb3abedd28b8d4940b07cdaf2b350677d551a6
souvik <souvik2805@gmail.com>
2020-09-22 13:49:19 +0530

    Mobx part- 1 in mobx-1/index.js

--- mobx-1/src/index.js
+++ mobx-1/src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { observable, autorun, action } from "mobx";
+
+class ShoppingCart {
+  @observable
+  items = [];
+
+  @action
+  add(item) {
+    this.items.push(item);
+  }
+}
+const mycart = new ShoppingCart();
+
+autorun(() => {
+  console.log(mycart.items.length);
+  console.log(mycart.items.map((cart) => cart.name));
+});
+
+mycart.add({ name: "computer", price: 200 });
+mycart.add({ name: "computer2", price: 200 });
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
